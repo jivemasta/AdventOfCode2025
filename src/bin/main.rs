e677fa49310e2b1d9da4fc2b4c81150de2a98acb
Terrance Heldman <jivemasta@gmail.com>
2025-12-09 18:34:46 -0500

    Start day 3

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,10 @@
-use advent_of_code_2025::{day1, day2};
+use advent_of_code_2025::{day1, day2, day3};
 
 fn main() {
-    println!("Day 1: {}", day1::part_one().unwrap());
-    println!("Day 2-1: {}", day2::part_one().unwrap());
-    println!("Day 2-2: {}", day2::part_two().unwrap());
+    //println!("Day 1: {}", day1::part_one().unwrap());
+    //println!("Day 2-1: {}", day2::part_one().unwrap());
+    //println!("Day 2-2: {}", day2::part_two().unwrap());
+    println!("Day 3-1: {}", day3::part_one().unwrap());
 }
 
 
---
+++ src/day3.rs
@@ -0,0 +1,12 @@
+pub fn part_one() -> Result<u32, Box<dyn std::error::Error>>{
+    todo!()
+}
+
+pub fn part_two() -> Result<u32, Box<dyn std::error::Error>>{
+    todo!()
+}
+
+#[cfg(test)]
+mod day_three_test {
+
+}
