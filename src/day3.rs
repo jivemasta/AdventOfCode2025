910038fa4b369b37f83ce79ceb16376e6b5befc9
Terrance Heldman <jivemasta@gmail.com>
2025-12-09 19:21:27 -0500

    change to just map

--- src/day3.rs
+++ src/day3.rs
@@ -3,7 +3,7 @@ use std::fs;
 pub fn part_one() -> Result<u32, Box<dyn std::error::Error>> {
     let final_joltage = fs::read_to_string("day_three_input.txt")?
         .lines()
-        .filter_map(get_joltage)
+        .map(get_joltage)
         .sum();
 
     Ok(final_joltage)
@@ -13,7 +13,7 @@ pub fn part_two() -> Result<u32, Box<dyn std::error::Error>> {
     todo!()
 }
 
-fn get_joltage(battery_bank: &str) -> Option<u32> {
+fn get_joltage(battery_bank: &str) -> u32 {
     todo!()
 }
 
@@ -25,27 +25,27 @@ mod day_three_test {
     fn joltage_test_1() {
         let joltage = get_joltage("987654321111111");
 
-        assert_eq!(joltage, Some(98));
+        assert_eq!(joltage, 98);
     }
 
     #[test]
     fn joltage_test_2() {
         let joltage = get_joltage("811111111111119");
 
-        assert_eq!(joltage, Some(89));
+        assert_eq!(joltage, 89);
     }
 
     #[test]
     fn joltage_test_3() {
         let joltage = get_joltage("234234234234278");
 
-        assert_eq!(joltage, Some(78));
+        assert_eq!(joltage, 78);
     }
 
     #[test]
     fn joltage_test_4() {
         let joltage = get_joltage("818181911112111");
 
-        assert_eq!(joltage, Some(92));
+        assert_eq!(joltage, 92);
     }
 }
